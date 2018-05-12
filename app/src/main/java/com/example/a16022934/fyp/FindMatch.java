3076eaa18abdb40cd66c80b18fafbdf81ce71bfd
Javier <16022934@myrp.edu.sg>
2018-05-13 02:06:27 +0800

    Created FindMatch.java, Match.java, MatchBaseAdapter.java. Created activity_find_match, item_row Changed Base App Color to Black

--- app/src/main/java/com/example/a16022934/fyp/FindMatch.java
+++ app/src/main/java/com/example/a16022934/fyp/FindMatch.java
@@ -3,13 +3,12 @@ package com.example.a16022934.fyp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FindMatch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_find_match);
 
     }
 }
--- app/src/main/java/com/example/a16022934/fyp/MatchBaseAdapter.java
+++ app/src/main/java/com/example/a16022934/fyp/MatchBaseAdapter.java
@@ -1,15 +1,14 @@
+
 package com.example.a16022934.fyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MatchBaseAdapter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.item_row);
     }
 }
