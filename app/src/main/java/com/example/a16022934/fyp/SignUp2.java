0bedef2aac9189e620bd70c7296337f21a96e37e
16022653 <16022653@myrp.edu.sg>
2018-05-22 19:41:34 +0800

    error in camera and gallery

--- app/src/main/java/com/example/a16022934/fyp/SignUp2.java
+++ app/src/main/java/com/example/a16022934/fyp/SignUp2.java
@@ -1,22 +1,38 @@
 package com.example.a16022934.fyp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SignUp2 extends AppCompatActivity {
+    ImageView ivProfile;
+    Integer REQUEST_CAMERA = 1, SELECT_FILE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_2);
+        ImageView ivProfile = (ImageView) findViewById(R.id.ivProfile);
 
         setTitle("SIGN UP");
 
         Button btn = findViewById(R.id.btnNext);
-
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SelectImage();
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +40,69 @@ public class SignUp2 extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+    }
+
+
+    //    protected void onActivityResult(int requestCode, int resultCode, Intent
+//            imageReturnedIntent) {
+//        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+//        ImageView ivProfile = (ImageView) findViewById(R.id.ivProfile);
+//        switch (requestCode) {
+//            case 0:
+//                if (resultCode == RESULT_OK) {
+//                    Uri selectedImage = imageReturnedIntent.getData();
+//                    ivProfile.setImageURI(selectedImage);
+//                }
+//
+//                break;
+//            case 1:
+//                if (resultCode == RESULT_OK) {
+//                    Uri selectedImage = imageReturnedIntent.getData();
+//                    ivProfile.setImageURI(selectedImage);
+//                }
+//                break;
+//        }
+//    }
+    private void SelectImage() {
+        final CharSequence[] items = {"Camera", "Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignUp2.this);
+        builder.setTitle("Add Image");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (items[i].equals("camera")) {
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 0);
+
+                } else if (items[i].equals("Gallery")) {
+
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, SELECT_FILE);
+
+                } else if (items[i].equals("Cancel")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent
+            data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CAMERA) {
+            Bundle bundle = data.getExtras();
+            final Bitmap bmp = (Bitmap) bundle.get("data");
+            ivProfile.setImageBitmap(bmp);
+        } else if (requestCode == SELECT_FILE) {
+            Uri selectedImageUri = data.getData();
+            ivProfile.setImageURI(selectedImageUri);
+        }
     }
 }
-
