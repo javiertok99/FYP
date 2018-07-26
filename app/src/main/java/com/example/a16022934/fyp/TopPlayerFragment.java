ab11379cdec3c82e41aa7bbebee436a30512ecd6
16022653 <16022653@myrp.edu.sg>
2018-07-26 22:21:37 +0800

    Topplayers

--- app/src/main/java/com/example/a16022934/fyp/TopPlayerFragment.java
+++ app/src/main/java/com/example/a16022934/fyp/TopPlayerFragment.java
@@ -4,14 +4,34 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class TopPlayerFragment extends Fragment {
+
+    private TextView tvName, tvAge, tvDescription, tvGender, tvLocation;
+    private ImageView ivProfile;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private static final String TAG = "TopPlayerFragment";
+
+    ArrayList<TopPlayers> alTops;
+
     public TopPlayerFragment() {
         // Required empty public constructor
     }
@@ -19,16 +39,40 @@ public class TopPlayerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("TopPlayers");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // TODO: Task 4 - Get the latest value from the dataSnapshot and display on the UI using the EditText message
+                // This method will get fired everytime the "message" value updates in the realtime database. We're getting our data back as a DataSnapshot
+                TopPlayers list = dataSnapshot.getValue(TopPlayers.class);
+                if (list != null) {
+                    tvName.setText(list.getName());
+                    tvAge.setText(list.getAge());
+                    tvDescription.setText(list.getDescription());
+                    tvLocation.setText(list.getLocation());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // This method will be invoked if there is any error
+                Log.e(TAG, "Database error occurred", databaseError.toException());
+            }
+        });
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_top_player, container, false);
-        ListView lvTopPlayers;
-        lvTopPlayers = view.findViewById(R.id.lvTopPlayers);
-        ArrayList<TopPlayers> alTops;
-        TopPlayerBaseAdapter tpAdapter;
-        alTops = new ArrayList<>();
-        alTops.add(new TopPlayers(R.drawable.profile, "Lin Dan", "Male", 34, "China"));
-        tpAdapter = new TopPlayerBaseAdapter(getActivity(), alTops);
-        lvTopPlayers.setAdapter(tpAdapter);
+//        ListView lvTopPlayers;
+//        lvTopPlayers = view.findViewById(R.id.lvTopPlayers);
+//        TopPlayerBaseAdapter tpAdapter;
+//        TopPlayers new1 = new TopPlayers(R.drawable.profile, tvName.getText().toString(), tvGender.getText().toString(), Integer.parseInt(tvAge.getText().toString()), tvLocation.getText().toString(),tvDescription.getText().toString());
+//        alTops.add(new1);
+//tpAdapter = new TopPlayerBaseAdapter(getActivity(), alTops);
+//        lvTopPlayers.setAdapter(tpAdapter);
+//        tpAdapter.notifyDataSetChanged();
         return view;
 
     }
--- app/src/main/java/com/example/a16022934/fyp/TopPlayers.java
+++ app/src/main/java/com/example/a16022934/fyp/TopPlayers.java
@@ -6,13 +6,15 @@ public class TopPlayers {
     private String gender;
     private int age;
     private String location;
+    private String description;
 
-    public TopPlayers(int res, String name, String gender, int age, String location) {
+    public TopPlayers(int res, String name, String gender, int age, String location, String description) {
         this.res = res;
         this.name = name;
         this.gender = gender;
         this.age = age;
         this.location = location;
+        this.description = description;
     }
 
     public int getRes() {
@@ -53,5 +55,13 @@ public class TopPlayers {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
