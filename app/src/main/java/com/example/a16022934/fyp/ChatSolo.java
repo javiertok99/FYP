009d878630c72fdcc44d39dd8e6a1ac12f9538d7
16022653 <16022653@myrp.edu.sg>
2018-08-02 21:03:20 +0800

    CHAT

---
+++ app/src/main/java/com/example/a16022934/fyp/ChatSolo.java
@@ -0,0 +1,72 @@
+package com.example.a16022934.fyp;
+
+import java.io.Serializable;
+
+public class ChatSolo implements Serializable{
+    private String senderName;
+    private String receiverName;
+    private String senderId;
+    private String receiverId;
+    private String msg;
+    private long timeStamp;
+
+    public ChatSolo(){
+    }
+
+    public ChatSolo(String senderName, String receiverName, String senderId, String receiverId, String msg, long timeStamp) {
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.msg = msg;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
